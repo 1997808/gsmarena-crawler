bb9c0dee059392322cc9d9cdf36e32aa6dbc5617
kstp0011 <phthienphu1210@gmail.com>
2023-11-01 00:02:35 +0700

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'HP/main'.
    
    Changes to be committed:
    	modified:   .gitignore
    	new file:   crawler/crawlPhonesSpec.py
    	modified:   inspectData.ipynb

---
+++ crawler/crawlPhonesSpec.py
@@ -0,0 +1,37 @@
+import yaml
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
+import time
+from selenium import webdriver
+from selenium.webdriver.edge.service import Service
+
+
+
+# Utility function
+def loadBrandsData(path, filename):
+    # load the brand data from csv
+    df = pd.read_csv(os.path.join(path, filename + ".csv"))
+    return df
+
+
+def createDriver():
+    # create the driver
+    service = Service('crawler\msedgedriver.exe')
+    service.start()
+    driver = webdriver.Remote(service.service_url)
+    return driver
+
+
+
+
+    
+
+
+if __name__ == '__main__':
+    with open('crawler\config.yaml', 'r') as f:
+        config = yaml.safe_load(f)
+
+    Brands_data = loadBrandsData(
+        config['SavePath'], config['BrandsListFileName'])
