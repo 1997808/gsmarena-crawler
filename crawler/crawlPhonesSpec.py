3ae6d85738b625f0f788c940eb2b8ce438183742
kstp0011 <phthienphu1210@gmail.com>
2023-11-04 11:05:48 +0700

    modified:   crawler/crawlPhonesSpec.py

--- crawler/crawlPhonesSpec.py
+++ crawler/crawlPhonesSpec.py
@@ -534,4 +534,4 @@ if __name__ == "__main__":
     with open('crawler\config.yaml', 'r') as f:
         config = yaml.safe_load(f)
 
-    crawlAllPhoneSpecs(config, start=10500, end=12000)
+    crawlAllPhoneSpecs(config, start=12000)
