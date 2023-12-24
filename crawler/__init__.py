8bd4eb146298b23081b5b2ffee04fd3e1962c84c
kstp0011 <phthienphu1210@gmail.com>
2023-12-24 12:36:18 +0700

    Moving files

--- crawler/__init__.py
+++
@@ -1,4 +0,0 @@
-from .crawlBranch import crawlBrandsData
-from .crawlDevicesUrl import crawlDevicesUrl
-from .crawlPhonesSpec import crawlAllPhoneSpecs
-from .concatPhoneSpecsData import concatPhoneSpecsData
